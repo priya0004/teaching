4889c70b38803dfdd38c35fae6dfdc54aa197b8e
priya0004 <priyavenugopal@berkeley.edu>
2025-01-23 00:47:57 -0800

    [sp25] week 1

--- src/content.js
+++ src/content.js
@@ -15,13 +15,13 @@ function content() {
                     <div id='links'>
                         <h4> Spring 2025 CS 61A Head TA </h4>
                         <p>
-                            <a className='link' target='_blank' rel='noreferrer'>
+                            <a className='link' target='_blank' rel='noreferrer' href='http://go.cs61a.org/discussion-attendance'>
                                 &#128204; Disc Attendance
                             </a> <br />
                             <a className='link' target='_blank' rel='noreferrer' href='https://docs.google.com/forms/d/e/1FAIpQLSdiL3MnoYt7ahtTMzzyQkVibuqJxU8linf7Nkkll7sI1lYFTQ/viewform?usp=sharing'>
                                 &#128394; Anonymous Feedback
                             </a> <br />
-                            <a className='link' target='_blank' rel='noreferrer'>
+                            <a>
                                 &#127911; Spotify Playlist
                             </a>
                         </p>
@@ -56,6 +56,20 @@ function content() {
                         <p id='announce'> &#129395; welcome to 61a! &#129395; </p>
                     </div>
                     <br />
+
+                    <div className='disc-container'>
+                        <div className='disc-row'>
+                            <div className='ind-disc-container'>
+                                <p> Week 1: Welcome </p>
+                                <a className='button' target='_blank' rel='noreferrer noopener' href='https://cs61a.org/disc/disc00/'>
+                                    disc00
+                                </a>
+                                <a className='button' target='_blank' rel='noreferrer noopener' href='https://docs.google.com/presentation/d/1gTW58OaxGsP2WCMqQn89ts-DcC3UzNWbsS0bvFnhZ3w/edit?usp=sharing'>
+                                    slides
+                                </a>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
